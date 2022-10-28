ba6109cf218b6c9feb63ae813a46601b09a7570a
khang <khang.job@gmail.com>
2022-10-28 16:08:08 +0700

    Setup graphql

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 var app = express();
 
@@ -17,7 +32,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
+var indexRouter = require('./routes/index.js')
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
